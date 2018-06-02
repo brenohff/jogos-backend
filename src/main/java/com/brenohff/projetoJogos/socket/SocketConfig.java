3c681d03a9aaa45e9802d5ffcc6c4a049e58493c
Breno Henrique Ferreira Franco <brenohff@gmail.com>
2018-06-02 20:39:17 -0300

    socket

---
+++ src/main/java/com/brenohff/projetoJogos/socket/SocketConfig.java
@@ -0,0 +1,24 @@
+package com.brenohff.projetoJogos.socket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class SocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/live");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/event").withSockJS();
+    }
+}
+
+
