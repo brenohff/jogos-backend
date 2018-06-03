e577838d5d05f10084896b113b2272788f144742
Breno Henrique Ferreira Franco <brenohff@gmail.com>
2018-06-03 18:17:29 -0300

    arrumando update

--- src/main/java/com/brenohff/projetoJogos/others/NaveRepository.java
+++ src/main/java/com/brenohff/projetoJogos/others/NaveRepository.java
@@ -1,16 +1,23 @@
 package com.brenohff.projetoJogos.others;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.brenohff.projetoJogos.domain.Nave;
 
-public interface NaveRepository extends JpaRepository<Nave, Long>{
-	
+public interface NaveRepository extends JpaRepository<Nave, Long> {
+
+	@Transactional
+	@Modifying
 	@Query("UPDATE Nave n SET n.escolhido = true WHERE n.id = :id")
 	public void atualizaNave(@Param("id") Long id);
-	
+
+	@Transactional
+	@Modifying
 	@Query("UPDATE Nave n SET n.escolhido = false")
 	public void resetaDados();
 
