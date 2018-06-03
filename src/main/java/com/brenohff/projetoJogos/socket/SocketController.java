29976a18d223207c1ac955dabac3d8bf750d8233
Breno Henrique Ferreira Franco <brenohff@gmail.com>
2018-06-03 18:00:34 -0300

    Ajustes socket

--- src/main/java/com/brenohff/projetoJogos/socket/SocketController.java
+++ src/main/java/com/brenohff/projetoJogos/socket/SocketController.java
@@ -1,5 +1,6 @@
 package com.brenohff.projetoJogos.socket;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -11,11 +12,17 @@ import com.brenohff.projetoJogos.domain.Nave;
 @Controller
 public class SocketController {
 
-	@MessageMapping("/event/1/sendNave")
-	@SendTo("/topic/event/1")
-	public Nave sendMessage(@Payload Nave nave,	SimpMessageHeaderAccessor headerAccessor) {
+	@MessageMapping("/event/{eventId}/sendNave")
+	@SendTo("/topic/event/{eventId}")
+	public Nave sendMessage(@DestinationVariable String eventId, @Payload Nave nave,	SimpMessageHeaderAccessor headerAccessor) {
 		headerAccessor.getSessionAttributes().put("nave", nave);
 		
 		return nave;
-	}	
+	}
+	
+	@MessageMapping("/event/{eventId}/naveEscolhida")
+	@SendTo("/topic/event/{eventId}")
+	public Nave naveEscolhida(@DestinationVariable String eventId, @Payload Nave nave) {
+		return nave;
+	}
 }
